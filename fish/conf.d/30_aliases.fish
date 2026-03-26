29416bb21fa7b9a8e01284ae084bfeb601a51c41
AlanHiram75 <alanhiram75@gmail.com>
2026-03-26 01:53:48 -0600

    add alias ss='systemctl suspend'

--- fish/conf.d/30_aliases.fish
+++ fish/conf.d/30_aliases.fish
@@ -61,7 +61,7 @@ alias lg='lazygit'       # <-- Uso rápido de 'lazygit'
 alias py='python3'       # <-- Uso rápido de 'python3'
 alias n='yazi'           # <-- Uso rápido de 'yazi'
 alias po='poweroff'      # <-- Apagar el sistema
-alias sl='sleep'         # <-- Pausar la ejecución temporalmente
+alias ss='systemctl suspend' # <-- Suspender el sistema
 alias postgrestart='sudo systemctl start postgresql.service'  # <-- Inicializar PostgreSQL
 # =============================================
 # 6.      INFORMACIÓN DEL SISTEMA
