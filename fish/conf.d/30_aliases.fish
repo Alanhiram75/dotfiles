3d5b084e0bcf0bd80c1e217312b9f5c814ca3d1c
AlanHiram75 <alanhiram75@gmail.com>
2026-04-14 16:39:40 -0600

    feat: añadir alias ge para gemini cli

--- fish/conf.d/30_aliases.fish
+++ fish/conf.d/30_aliases.fish
@@ -63,6 +63,7 @@ alias n='yazi'           # <-- Uso rápido de 'yazi'
 alias po='poweroff'      # <-- Apagar el sistema
 alias ss='systemctl suspend; and exit' # <-- Suspender el sistema y cerrar terminal
 alias lc='ollama launch claude' # <-- Lanzar Claude con Ollama
+alias ge='gemini'              # <-- Lanzar Gemini CLI
 alias nlc='cd ~/notes; and lc'  # <-- Ir a notes y lanzar Claude
 alias postgrestart='sudo systemctl start postgresql.service'  # <-- Inicializar PostgreSQL
 alias ui='cd inir && ./setup'                                  # <-- Update inir
